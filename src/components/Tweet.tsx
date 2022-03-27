1bd1b75bd4f1c1d9de642e389f1901c70121663f
andybrooker <dy.brooker@gmail.com>
2022-03-27 15:53:19 +0100

    add

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,10 +1,12 @@
 import React, {useRef, useCallback} from 'react'
 import TweetText from './TweetText'
-import { TweetV2, ApiV2Includes, MediaObjectV2 } from 'twitter-api-v2'
-import { Card, Box, CardMedia } from '@mui/material'
+import { TweetV2, TweetEntitiesV2, TweetEntityUrlV2, ApiV2Includes, MediaObjectV2 } from 'twitter-api-v2'
+import { Card, Box, CardMedia, Typography } from '@mui/material'
 import { TweetInfo } from './TweetInfo'
 import TweetMedia from './edition/user/TweetMedia'
 import { toPng } from 'html-to-image';
+import Image from 'next/image'
+import { alpha, palette } from '@mui/system'
 
 
 export default function Tweet({ data, includes, isLoading }: { data: TweetV2, includes: ApiV2Includes, isLoading: boolean }) {
@@ -21,13 +23,16 @@ export const TweetContent = ({ data, includes }: { data: TweetV2, includes: ApiV
 
 
     //One Link Case
-    if (data.entities && data.entities.urls && !data.attachments && (!data.referenced_tweets || data.referenced_tweets.find(object => object.type === 'replied_to'))) {
+    if (data?.entities?.urls?.length == 1 && !data.attachments && (!data.referenced_tweets || data.referenced_tweets.find(object => object.type === 'replied_to'))) {
 
         data.text = data.text.replace(data.entities.urls[0].url, '')
 
         return (
             <>
-                <Box><TweetText tweet={data} /></Box>
+                <Box>
+                    <TweetText tweet={data} />
+                    <TweetLink entity={data?.entities?.urls[0]}/>
+                </Box>
             </>
         )
 
@@ -75,5 +80,24 @@ export const TweetContent = ({ data, includes }: { data: TweetV2, includes: ApiV
         )
 
     }
+
+}
+
+const TweetLink = ({entity} : { entity: TweetEntityUrlV2}) => {
+
+    const hasPreview = entity.hasOwnProperty('title');
+
+    return (
+        <Box sx={{display: 'flex', border: '1px solid', borderColor: 'divider', borderRadius: '8px', width: '100%', height: '100px'}}>
+            <Box sx={{overflow: 'hidden', borderTopLeftRadius: '7px', borderBottomLeftRadius: '7px', width: '100px', height: '100%', minWidth: '100px', position: 'relative'}}>
+                <Image alt={entity.description} src={entity?.images[0].url} layout='fill' objectFit='cover' objectPosition='center'/>
+            </Box>
+            <Box sx={{backgroundColor: 'action.hover', borderTopRightRadius: '7px', borderBottomRightRadius: '7px', p: 1, flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly'}}>
+                <Typography sx={{fontSize: '0.75rem', fontWeight: 400, display: '-webkit-box', WebkitBoxOrient: 'vertical',WebkitLineClamp: 1, overflow: 'hidden', textOverflow: 'ellipsis'}}>{entity.title}</Typography>
+                <Typography sx={{fontSize: '0.75rem', fontWeight: 300, display: '-webkit-box', WebkitBoxOrient: 'vertical',WebkitLineClamp: 2, overflow: 'hidden', textOverflow: 'ellipsis'}}>{entity.description}</Typography>
+                <Typography sx={{fontSize: '0.75rem', fontWeight: 300, color: 'text.secondary'}}>{entity.display_url}</Typography>
+            </Box>
+        </Box>
+    )
 
 }
